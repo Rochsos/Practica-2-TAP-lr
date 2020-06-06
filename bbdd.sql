d16c0d96e106c5f3e281396d524909c021928a87
Rocío <rociioo1998@gmail.com>
2020-06-06 13:22:14 +0200

    Modificando la BBDD embebida a persistente

--- bbdd.sql
+++ bbdd.sql
@@ -1,33 +1,16 @@
-DROP TABLE IF EXISTS usuario;
-CREATE TABLE usuario(
-	IdUsuario bigint auto_increment,
-	nombre varchar(50) not null,
-	Contrasenia varchar(50) not null,
-	primary key(IdUsuario));
+DROP TABLE IF EXISTS LISTA_TAREAS;
+CREATE TABLE LISTA_TAREAS(
+	ID BIGINT NOT NULL,
+	NOMBRE VARCHAR(255) NOT NULL,
+	PRIMARY KEY(ID));
 
-INSERT INTO usuario (IdUsuario, nombre, Contrasenia) VALUES (1, 'Luisa', 'tapufvluisa');
-INSERT INTO usuario (IdUsuario, nombre, Contrasenia) VALUES (2, 'Rocio', 'tapufvrocio');
-
-DROP TABLE IF EXISTS lista_tareas;
-CREATE TABLE lista_tareas(
-	IdListaTareas bigint auto_increment,
-	nombre varchar(50) not null,
-	IdUsuario bigint not null,
-	primary key(IdListaTareas));
-	
-INSERT INTO lista_tareas (IdListaTareas, nombre, IdUsuario) VALUES (1, 'trabajo', 1);
-INSERT INTO lista_tareas (IdListaTareas, nombre, IdUsuario) VALUES (2, 'relaciones internacionales', 2);
-
-DROP TABLE IF EXISTS tarea;
-CREATE TABLE tarea(
-	IdTarea bigint auto_increment,
-	nombre varchar(50) not null,
-	Descripcion varchar(100) not null,
-	Prioridad varchar(50) not null,
-	DeadLine date,
-	EstadoTarea varchar(50) not null,
-	IdLista bigint not null,
-	primary key(IdTarea));
-	
-INSERT INTO tarea (IdTarea, nombre, Descripcion, Prioridad, EstadoTarea, IdLista) VALUES (1, 'Tarea 1', 'tarea 1 de la lista 1', 'alta', 'completada', 1);
-INSERT INTO tarea (IdTarea, nombre, Descripcion, Prioridad, EstadoTarea, IdLista) VALUES (2, 'Tarea 1', 'tarea 1 de la lista 2','baja', 'por completar', 2);
+DROP TABLE IF EXISTS TAREA;
+CREATE TABLE TAREA(
+	ID BIGINT NOT NULL,
+    DEADLINE DATE NOT NULL,
+    DESCRIPCION VARCHAR(255) NOT NULL,
+    ESTADO_TAREA VARCHAR(255) NOT NULL,
+    NOMBRE VARCHAR(255) NOT NULL,
+    PRIORIDAD VARCHAR (255) NOT NULL,
+    LISTATAREAS_ID BIGINT NOT NULL,
+	PRIMARY KEY(ID));
